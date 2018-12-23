de0030f7d94ba0541cb3620d60801c95d585ca98
서경원/모니터링플랫폼개발팀/NE <kyeongwon.seo@nhnent.com>
2018-12-23 17:07:35 +0900

    implemented kfkcui UI and added topic option

--- kfkcui/main.go
+++ kfkcui/main.go
@@ -15,25 +15,85 @@ import (
 const (
 	addrSeparator = ","
 	defaultAddrs  = "127.0.0.1:9092"
-	viewLeftSide  = "leftSide"
+	defaultTopic = "test"
+	viewProducer  = "viewProducer"
+	viewConsumer  = "viewConsumer"
 )
 
-var addrs = flag.String("addr", "", "kafka broker address")
+var (
+	addrs = flag.String("addr", defaultAddrs, "kafka broker address")
+	topic = flag.String("topic", defaultTopic, "kafka topic")
+)
+type KfkGui struct {
+	producer sarama.SyncProducer
+	consumer sarama.Consumer
+	topic string
+}
 
-func produce(producer sarama.SyncProducer) {
-	log.Println("Start producing")
-	msg := sarama.ProducerMessage{Topic: "test", Value: sarama.StringEncoder("SUCCESS")}
-	if _, _, err := producer.SendMessage(&msg); err != nil {
-		log.Panicln(err)
+func NewKfkGui(addrs []string, topic string) *KfkGui {
+	producer, err := sarama.NewSyncProducer(addrs, nil)
+	utils.MustNotErr(err)
+
+	consumer, err := sarama.NewConsumer(addrs, nil)
+	utils.MustNotErr(err)
+
+	return &KfkGui{
+		producer: producer,
+		consumer: consumer,
+		topic: topic,
 	}
 }
 
-func consume(consumer sarama.Consumer) {
-	log.Println("Start consuming")
+func (k *KfkGui) Close() {
+	k.producer.Close()
+	k.consumer.Close()
+}
 
-	partition, err := consumer.ConsumePartition("test", 0, sarama.OffsetNewest)
+func (k *KfkGui) Layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView(viewProducer, 0, 0, maxX/2, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		if _, err := g.SetCurrentView(viewProducer); err != nil {
+			return err
+		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		v.Title = "Producer"
+		v.Editable = true
+	}
+
+	if v, err := g.SetView(viewConsumer, maxX/2, 0, maxX, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "Consumer"
+		v.Autoscroll = true
+
+		go k.consume(g, v)
+	}
+
+	return nil
+}
+
+func (k *KfkGui) produce(g *gocui.Gui, v *gocui.View) error {
+	v.EditNewLine()
+	buf := v.ViewBufferLines()
+	data := buf[len(buf)-1]
+	msg := sarama.ProducerMessage{Topic: k.topic, Value: sarama.StringEncoder(data)}
+	if _, _, err := k.producer.SendMessage(&msg); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (k *KfkGui) consume(g *gocui.Gui, out *gocui.View) error {
+	partition, err := k.consumer.ConsumePartition(k.topic, 0, sarama.OffsetNewest)
 	if err != nil {
-		log.Panicln(err)
+		return err
 	}
 	defer partition.Close()
 
@@ -43,57 +103,49 @@ func consume(consumer sarama.Consumer) {
 	ConsumerLoop:for {
 		select {
 		case msg := <- partition.Messages():
-			log.Printf("%s\n", msg.Value)
+			g.Update(func(_ *gocui.Gui) error {
+				fmt.Fprintf(out, "%s\n", msg.Value)
+				return nil
+			})
 		case <- signals:
 			break ConsumerLoop
 		}
 	}
+
+	return nil
+}
+
+func doNothing(_ *gocui.Gui, _ *gocui.View) error {
+	return nil
+}
+
+func (k *KfkGui) keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding(viewProducer, gocui.KeyEnter, gocui.ModNone, k.produce); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding(viewProducer, gocui.KeyArrowUp, gocui.ModNone, doNothing); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding(viewProducer, gocui.KeyArrowDown, gocui.ModNone, doNothing); err != nil {
+		return err
+	}
+	return nil
 }
 
 func quit(_ *gocui.Gui, _ *gocui.View) error {
 	return gocui.ErrQuit
 }
 
-func keybindings(g *gocui.Gui) error {
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		return err
-	}
-	return nil
-}
-
-func layout(g *gocui.Gui) error {
-	_, maxY := g.Size()
-	if v, err := g.SetView(viewLeftSide, 0, 0, 10, maxY); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		if _, err := g.SetCurrentView(viewLeftSide); err != nil {
-			return err
-		}
-		v.Highlight = true
-		v.SelBgColor = gocui.ColorGreen
-		v.SelFgColor = gocui.ColorBlack
-		fmt.Fprintln(v, viewLeftSide)
-		fmt.Fprintln(v, "wait...")
-	}
-	return nil
-}
-
 func main() {
 	flag.Parse()
-
-	if *addrs == "" {
-		*addrs = defaultAddrs
-	}
+	log.Printf("%s %s\n", *addrs, *topic)
 
 	parsedAddrs := strings.Split(*addrs, addrSeparator)
-	producer, err := sarama.NewSyncProducer(parsedAddrs, nil)
-	utils.MustNotErr(err)
-	defer producer.Close()
-
-	consumer, err := sarama.NewConsumer(parsedAddrs, nil)
-	utils.MustNotErr(err)
-	defer consumer.Close()
+	kfkGui := NewKfkGui(parsedAddrs, *topic)
+	defer kfkGui.Close()
 
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	utils.MustNotErr(err)
@@ -101,9 +153,9 @@ func main() {
 
 	g.Cursor = true
 
-	g.SetManagerFunc(layout)ß
-
-	utils.MustNotErr(keybindings(g))
+	g.SetManager(kfkGui)
+	err = kfkGui.keybindings(g)
+	utils.MustNotErr(err)
 
 	if err := g.MainLoop(); err != gocui.ErrQuit {
 		log.Panicln(err)
